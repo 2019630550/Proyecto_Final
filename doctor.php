fffa1ab7009c0763e0a9274faab3073c93666646
2019630550 <155586639+2019630550@users.noreply.github.com>
2024-07-04 14:28:54 -0600

    Add files via upload

---
+++ doctor.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Proyecto de base de datos"/>
+    <meta name="author" content="Veruete Hernandez Bryan David"/>
+    <meta name="author" content="Tadeo Martínez Xiadani Alexahyatt "/>
+    <meta name="author" content="Reyes León José Ramón "/>
+    <meta name="keywords" content="HTML, CSS, JS, SQL, PHP"/>
+    <title>Doctor</title>
+</head>
+<body>
+    <a href="ver_cita.php">Ver citas agendadas</a>
+</body>
+</html>
