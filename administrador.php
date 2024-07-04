2bfa9e8bfdabb27ec2bc1e4319be61fdbf4440e6
2019630550 <155586639+2019630550@users.noreply.github.com>
2024-07-04 14:21:01 -0600

    Create administrador.php

---
+++ administrador.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Proyecto de base de datos"/>
+    <meta name="author" content="Veruete Hernandez Bryan David"/>
+    <meta name="author" content="Tadeo Martínez Xiadani Alexahyatt "/>
+    <meta name="author" content="Reyes León José Ramón "/>
+    <meta name="keywords" content="HTML, CSS, JS, SQL, PHP"/>
+    <title>Administrador</title>
+</head>
+<body>
+    <a href="doctores.php">doctores</a>
+    <a href="pacientes.php">pacientes</a>
+    <a href="validar.php">Recepcionista</a>
+</body>
+</html>
